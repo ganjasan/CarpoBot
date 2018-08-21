06bbe3f2d0ba8bd5f0daf61a9e02c824571bdc4f
Artem <konuchovartem@gmail.com>
2018-08-21 23:04:03 +0300

    bug test

--- carpo_bot.py
+++ carpo_bot.py
@@ -105,7 +105,7 @@ def send_nearest_places(message):
 
     for place_type in places.keys():
         button = telebot.types.InlineKeyboardButton(text = place_type, callback_data = place_type + "#" + str(location.latitude) + "#" + str(location.longitude))
-        bot.send_message(message.chat.id, place_type + "#" + str(location.latitude) + "#" + str(location.longitude))
+        bot.send_message(message.chat.id, "Все заведения" + "#" + str(59.929065) + "#" + str(30.471106))
         keyboard.add(button)
 
     bot.send_message(message.chat.id, messages.repeat_messages['ru']['place_type_repeat'], reply_markup=keyboard)
