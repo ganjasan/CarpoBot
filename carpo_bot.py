8e179975933dc226b1e62bcdf4b08cbf9c28af7a
Artem <konuchovartem@gmail.com>
2018-08-21 17:27:57 +0300

    bug fix

--- carpo_bot.py
+++ carpo_bot.py
@@ -87,7 +87,7 @@ def repeat_all_text_messages(message):
 @bot.message_handler(content_types=["location"])
 def send_nearest_places(message):
     location = message.location
-    bot.send_location(message.chat.id, location.longitude, location.latitude)
+    bot.send_location(message.chat.id, location.latitude, location.longitude)
 
 
 
